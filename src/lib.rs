e17e43118d70c5cdc5c58c0047ff5c89560cc27c
seabass <sebastian.goodman@gmail.com>
2025-03-01 20:48:34 -0800

    fix: res again

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use bevy::window::WindowResolution;
 use wasm_bindgen::prelude::*;
 
-const X_EXTENT: f32 = 800.;
+const X_EXTENT: f32 = 768.;
 const Y_EXTENT: f32 = X_EXTENT;
 
 #[wasm_bindgen(start)]
